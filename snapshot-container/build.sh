7371075698b69c0556757f401e5ce14e86450b5c
Matt Kelsey <kelseym@mir.wustl.edu>
2024-01-26 09:22:37 -0600

    Updated snapshot container version to 0.4.0

--- snapshot-container/build.sh
+++ snapshot-container/build.sh
@@ -1,4 +1,4 @@
 cp Dockerfile.base Dockerfile && \
 ./command2label.py command.json >> Dockerfile && \
-docker build -t xnat/data-snapshot:0.1 .
+docker build -t xnat/data-snapshot:0.4 .
 rm Dockerfile 
